1666cd6deb437490d9df1c138a215b39cd0e82f9
ajaezokingsley <ajaezokingsley@gmail.com>
2022-01-29 22:07:38 -0800

    refactor of escape rules in wishlist page

--- components/widgets/wishlist/Wishlist.js
+++ components/widgets/wishlist/Wishlist.js
@@ -24,7 +24,10 @@ const Wishlist = () => {
       <h2 className="mb-3 mt-2">Wish List</h2>
       <div>
         {wishlistCart?.map(product => (
-          <section className={[cartStyles.cart_item_container].join(' ')}>
+          <section
+            className={[cartStyles.cart_item_container].join(' ')}
+            key={product.id}
+          >
             <div className={cartStyles.image}>
               <Image src={product.image} height={120} width={120} />
             </div>
